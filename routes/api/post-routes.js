2ae78c5f8fc2aa842dca4e2cae4a6e0207f313f5
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-01-11 23:40:39 -0500

    refactored

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -102,35 +102,11 @@ router.post("/", (req, res) => {
 
 // PUT api/posts/upvote
 router.put("/upvote", (req, res) => {
-  // Create the post
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id,
-  })
-    .then(() => {
-      // Find the post that was just voted on
-      return Post.findOne({
-        where: { id: req.body.post_id },
-        // What we want to see
-        attributes: [
-          "id",
-          "post_url",
-          "title",
-          "created_at",
-          // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
-          [
-            sequelize.literal(
-              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-            ),
-            "vote_count",
-          ],
-        ],
-      });
-    })
-    .then((dbPostVoteData) => {
-      res.json(dbPostVoteData);
-    })
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then((updatedPostData) => res.json(updatedPostData))
     .catch((err) => {
+      console.log(err);
       res.status(400).json(err);
     });
 });
